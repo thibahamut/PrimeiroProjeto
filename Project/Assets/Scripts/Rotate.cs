d3cd75e4ed7768381269c841fe593964eadfdba2
Robson Matos <robson@timepix.com.br>
2013-09-13 22:19:18 -0300

    ale_comm

---
+++ Project/Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour 
+{
+	public float speed = 5;
+
+    // ############################################################################################ // #UNITY FUNCTION
+    void Start()
+    {
+        //Debug.Log("Teste commit");
+    }
+    // ############################################################################################ // #UNITY FUNCTION
+	void Update () 
+	{
+		transform.Rotate( (Vector3.up * speed) * Time.deltaTime , Space.World );
+	}
+}
---
+++ Project/Assets/Scripts/RotateCapsule.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCapsule : MonoBehaviour {
+
+    public float speed = 5;
+
+    // ############################################################################################ // #UNITY FUNCTION
+	void Start ()
+    {
+	    
+	}
+    // ############################################################################################ // #UNITY FUNCTION
+	void Update ()
+    {
+        transform.Rotate((Vector3.right * speed) * Time.deltaTime, Space.World);
+	}
+    // ############################################################################################ // 
+}
